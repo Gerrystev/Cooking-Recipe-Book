335e13c363155f98a363b42155f11f0db0c95736
Crucios <57003452+Crucios@users.noreply.github.com>
2020-06-05 15:46:33 +0700

    Key for Firebase

---
+++ src/app/credentials.ts
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyC8rtBZx_2EuYOnyN3p8FhWV6nAp857cd8",
+    authDomain: "cooking-mama-5c4e1.firebaseapp.com",
+    databaseURL: "https://cooking-mama-5c4e1.firebaseio.com",
+    projectId: "cooking-mama-5c4e1",
+    storageBucket: "cooking-mama-5c4e1.appspot.com",
+    messagingSenderId: "562783082897",
+    appId: "1:562783082897:web:bbc7866bad623e234a6230",
+    measurementId: "G-Z04H2QQ92Z"
+};
