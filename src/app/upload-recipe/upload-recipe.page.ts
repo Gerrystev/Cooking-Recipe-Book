dd410baab255ab7e9a92823a69c9560f10193f6d
Crucios <57003452+Crucios@users.noreply.github.com>
2020-06-09 13:30:18 +0700

    Upload Page Front End

--- src/app/upload-recipe/upload-recipe.page.ts
+++ src/app/upload-recipe/upload-recipe.page.ts
@@ -12,4 +12,14 @@ export class UploadRecipePage implements OnInit {
   ngOnInit() {
   }
 
+  add_ingredients_field(){
+    let markup = "<ion-row><ion-col><ion-input class=\"ingredients-field\" placeholder=\"1kg Sugar\"></ion-input></ion-col></ion-row>";
+    document.getElementById("ingredients-section").outerHTML += markup;
+    console.log(document.getElementById("ingredients-section"));
+  }
+  
+  add_directions_field(){
+    let markup = "<ion-row><ion-col><ion-input class=\"ingredients-field\" placeholder=\"Boil the water for 10 minutes\"></ion-input></ion-col></ion-row>";
+    document.getElementById("directions-section").outerHTML += markup;
+  }
 }
