74c3cf8d0e188dc572940fada033e17da8fca056
Crucios <57003452+Crucios@users.noreply.github.com>
2020-06-09 16:50:29 +0700

    Update login.page.ts

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -24,7 +24,7 @@ export class LoginPage implements OnInit {
   ngOnInit() {
   }
   login(){
-    if(this.password == "" || this.username == ""){
+    if(this.password != "" || this.username != ""){
       console.log(this.password);
       console.log(this.username);
       this.cariuser2 = this.getUser().valueChanges();
