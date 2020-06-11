e47f99f70c9e8be42d0ad090232462b79cc18dfe
Crucios <57003452+Crucios@users.noreply.github.com>
2020-06-11 11:46:48 +0700

    Working Login

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -3,6 +3,7 @@ import { Router } from '@angular/router';
 import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
 import { User } from '../models/user.model';
+import { LoadingController } from '@ionic/angular';
 
 
 @Component({
@@ -16,8 +17,11 @@ export class LoginPage implements OnInit {
   password:string;
   cariuser : User;
   cariuser2 : Observable<User>
-  constructor(private route:Router,
-    private fireStore:AngularFirestore) { 
+  loading: HTMLIonLoadingElement;
+  constructor(
+    private route:Router,
+    private fireStore:AngularFirestore,
+    private loadingController : LoadingController) { 
     this.resepCol = this.fireStore.collection<User>('User');
   }
 
@@ -27,33 +31,43 @@ export class LoginPage implements OnInit {
     if(typeof(this.password) !== "undefined" && typeof(this.username) !== "undefined"){
       console.log(this.password);
       console.log(this.username);
+      this.presentLoading();
       this.cariuser2 = this.getUser().valueChanges();
       this.cariuser2.subscribe(user =>{
         console.log(user);
         this.cariuser = user;
-      })
-      console.log(this.cariuser2);
-      if(typeof(this.cariuser2) === "undefined"){
-        alert("Username tidak ditemukan");
-      }
-      else{
-        if(this.password === this.cariuser.password){
-          this.route.navigate(['/tabs/home']);
+        this.loading.dismiss();
+        if(typeof(this.cariuser2) === "undefined"){
+          alert("Username tidak ditemukan");
         }
         else{
-          this.username = "";
-          this.password = "";
-          alert("Password or Username salah!");
+          if(this.password === this.cariuser.password){
+            this.route.navigate(['/tabs/home']);
+          }
+          else{
+            this.username = "";
+            this.password = "";
+            alert("Password or Username salah!");
+          }
         }
-      }
+      });
     }
     else{
       alert("Fill in the Form");
     }
   }
+
+  async presentLoading() {
+    this.loading = await this.loadingController.create({
+      message: 'Logging in...'
+    });
+    return this.loading.present();
+  }
+
   signup(){
     this.route.navigate(['/signup']);
   }
+  
   getUser() : AngularFirestoreDocument<User>{
     return this.fireStore.collection('User').doc(this.username);
   }
